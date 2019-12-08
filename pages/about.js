ed327ef3c8cc34feda23ce6f5d8180b0a567434c
kwanjack <jack.ks.kwan@gmail.com>
2019-12-07 17:21:43 -0800

    Delete unused dev pages.

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/MyLayout';
-
-export default function About() {
-  return (
-    <Layout>
-      <p> By Khoa Luong, Allen Liu, Jack Kwan.</p>
-    </Layout>
-  );
-}
